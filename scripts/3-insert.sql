dd3aeb0ba6f213ff20d49a8dc5b1ab2655c8ef07
Ramon Xavier <ramon1234rxm@gmail.com>
2025-06-17 10:46:42 -0300

    Refactor SQL insert statements for customers to include explicit column names and return the generated ID.

--- scripts/3-insert.sql
+++ scripts/3-insert.sql
@@ -1,3 +1,7 @@
 \c database_lab;
 
 INSERT INTO customers VALUES(DEFAULT, 'Ramon', 'Xavier', DEFAULT, 'ramon@gmail.com');
+
+INSERT INTO customers (first_name, last_name, email) VALUES ('Ramon', 'Xavier', 'ramon@gmail.com');
+
+INSERT INTO customers (first_name, last_name, email) VALUES ('Ramon', 'Xavier', 'ramon@gmail.com') RETURNING id;
