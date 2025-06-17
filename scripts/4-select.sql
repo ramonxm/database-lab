a8c2adbb7ada58642cf8035b775c688066b1f35a
Ramon Xavier <ramon1234rxm@gmail.com>
2025-06-17 10:14:16 -0300

    Enhance SQL script to include aliasing for customer fields in selection query

--- scripts/4-select.sql
+++ scripts/4-select.sql
@@ -4,3 +4,6 @@ SELECT id, first_name, last_name, email FROM customers;
 
 -- ALL
 SELECT * FROM customers;
+
+-- AS
+SELECT id AS ID, first_name as firstName, last_name as lastName, email as emailAddress FROM customers;
